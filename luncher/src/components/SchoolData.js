06746a80e3bb398c0206c5063caca7761b0240a1
Alberto Galvan <astrometrics@Unimatrix-Zero.lan1>
2019-10-22 16:40:06 -0500

    school data formik admin form complete

--- luncher/src/components/SchoolData.js
+++ luncher/src/components/SchoolData.js
@@ -1,5 +1,9 @@
-////These are the test objects before we use an api.
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
+////These are the test objects before we use an api.
 export default [
     {
         schoolName: "Johnston Elementary School",
@@ -22,3 +26,142 @@ export default [
     }
 
 ];
+
+////// This is the schoolform data that admin creates
+const SchoolData = ({ values, touched, errors, status }) => {
+
+    const [schoolData, setSchoolData] = useState([])
+
+    useEffect(() => {
+        status && setSchoolData(schoolData => [...schoolData, status])
+    }, [status])
+
+    return (
+        <div className='school-data'>
+            <Form>
+                //schoolName
+                <Field type='text' name='schoolName' placeholder='School Name' />
+                {touched.schooName && errors.schoolName && (
+                    <p className='error'>{errors.schoolName}</p>
+                )}
+                //address
+                <Field type='text' name='address' placeholder='Address' />
+                {touched.address && errors.address && (
+                    <p className='error'>{errors.address}</p>
+                )}
+                //city
+                <Field type='text' name='city' placeholder='City' />
+                {touched.city && errors.city && (
+                    <p className='error'>{errors.city}</p>
+                )}
+                //state
+                <Field component='select' className='state-select' name='state' />
+                <option>Choose Your State</option>
+                <option value='AL'>Alabama</option>
+                <option value='AK'>Alaska</option>
+                <option value='AZ'>Arizona</option>
+                <option value='AR'>Arkansas</option>
+                <option value='CA'>California</option>
+                <option value='CO'>Colorado</option>
+                <option value='CT'>Connecticut</option>
+                <option value='DC'>District of Columbia</option>
+                <option value='DE'>Delaware</option>
+                <option value='FL'>Florida</option>
+                <option value='GA'>Georgia</option>
+                <option value='HI'>Hawaii</option>
+                <option value='ID'>Idaho</option>
+                <option value='IL'>Illinois</option>
+                <option value='IN'>Indiana</option>
+                <option value='IA'>Iowa</option>
+                <option value='KS'>Kansas</option>
+                <option value='KY'>Kentucky</option>
+                <option value='LA'>Louisiana</option>
+                <option value='ME'>Maine</option>
+                <option value='MD'>Maryland</option>
+                <option value='MA'>Massachusetts</option>
+                <option value='MI'>Michigan</option>
+                <option value='MN'>Minnesota</option>
+                <option value='MS'>Mississippi</option>
+                <option value='MO'>Missouri</option>
+                <option value='MT'>Montana</option>
+                <option value='NE'>Nebraska</option>>
+                <option value='NV'>Nevada</option>
+                <option value='NH'>New Hampshire</option>
+                <option value='NJ'>New Jersey</option>
+                <option value='NM'>New Mexico</option>
+                <option value='NY'>New York</option>
+                <option value='NC'>North Carolina</option>
+                <option value='ND'>North Dakota</option>
+                <option value='OH'>Ohio</option>
+                <option value='OK'>Oklahoma</option>
+                <option value='OR'>Oregon</option>
+                <option value='PA'>Pennsylvania</option>
+                <option value='RI'>Rhode Island</option>
+                <option value='SC'>South Carolina</option>
+                <option value='SD'>South Dakota</option>
+                <option value='TN'>Tennessee</option>
+                <option value='TX'>Texas</option>
+                <option value='UT'>Utah</option>
+                <option value='VT'>Vermont</option>
+                <option value='VA'>Virginia</option>
+                <option value='WA'>Washington</option>
+                <option value='WV'>West Virginia</option>
+                <option value='WI'>Wisconsin</option>
+                <option value='WY'>Wyoming</option>
+                {touched.state && errors.state && (
+                    <p className='error'>{errors.state}</p>
+                )}
+                ///zipcode
+                <Field type='text' name='zip' placeholder='Zip Code' />
+                {touched.zip && errors.zip && (
+                    <p className='error'>{errors.zip}</p>
+                )}
+                ////funds needed
+                <Field type='text' name='fundsNeeded' placeholder='Fund Goal' />
+                {touched.fundsNeeded && errors.fundsNeeded && (
+                    <p className='error'>{errors.fundsNeeded}</p>
+                )}
+                ////funds fundsraised    
+                <Field type='text' name='fundsRaised' placeholder='Funds Raised' />
+                {touched.fundsRaised && errors.fundsRaised && (
+                    <p className='error'>{errors.fundsRaised}</p>
+                )}
+            </Form>
+        </div >
+    );
+};
+
+const FormikSchoolData = withFormik({
+    mapPropsToValues({ schoolName, address, city, state, zip, fundsNeeded, fundsRaised }) {
+        return {
+            schoolName: schoolName || '',
+            address: address || '',
+            city: city || '',
+            state: state || '',
+            zip: zip || '',
+            fundsNeeded: fundsNeeded || '',
+            fundsRaised: fundsRaised || ''
+        };
+    },
+    validationSchema: Yup.object().shape({
+        schoolName: Yup.string().required(),
+        address: Yup.string().required(),
+        city: Yup.string().required(),
+        state: Yup.string().required('Please Select One'),
+        zip: Yup.string().required(),
+        fundsNeeded: Yup.string().required(),
+        fundsRaised: Yup.string().required(),
+    }),
+    handleSubmit(values, { setStatus }) {
+        axios.post('', values)
+            .then(res => { setStatus(res.data); })
+            .catch(err => console.log(err.response));
+    }
+
+})(SchoolData);
+
+//////////////uncomment the line below!!!!
+
+// export default FormikSchoolData;
+// console.log('this better work', FormikSchoolData);
+
--- luncher/src/components/SchoolList.js
+++ luncher/src/components/SchoolList.js
@@ -1,4 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ padding: 1rem;
+ margin: 5% auto;
+ width: 25%;
+ height: 300px;
+ background-color: #F8F8F8;
+ opacity: 0.7;
+ border: 1px solid black;
+ border-radius: 10px;
+`;
+
+const H1 = styled.h1`
+font-size: 1.9rem;
+font-weight: 400;
+`;
+
+const P = styled.p`
+font-size: 1rem;
+`;
+
+const ActionButton = styled.button`
+ margin: 0 5px;
+ padding: 8px 14px;
+ background: #74B9FF;
+ cursor: pointer;
+ border: 1px solid black;
+ outline: 0;
+ font-weight: 800;
+ border-radius; 10px;
+ :hover {
+     opacity: 0.8
+ }
+ ${props => (props.type === 'needed') ? `background: #74B9FF` : null}
+ ${props => (props.type === 'raised') ? `background: #FFFA7A` : null}
+ ${props => (props.type === 'donate') ? `background: #FFAD72` : null}
+ `;
 
 const SchoolList = props => {
 
@@ -6,16 +48,16 @@ const SchoolList = props => {
         <>
             {props.schoolList.map((school, index) => {
                 return (
-                    <div className='school' key={index}>
-                        <h2>{school.schoolName}</h2>
-                        <p>{school.address}</p>
-                        <p>{school.city}</p>
-                        <p>{school.state}</p>
-                        <p>{school.zip}</p>
-                        <p>{school.fundsNeeded}</p>
-                        <p>{school.fundsRaised}</p>
-
-                    </div>
+                    <Card key={index}>
+                        <H1>{school.schoolName}</H1>
+                        <P>{school.address}</P>
+                        <P>{school.city}, {school.state}. {school.zip}</P>
+                        <ActionButton type='needed'>{school.fundsNeeded} Funds Needed</ActionButton>
+                        <ActionButton type='raised'>{school.fundsRaised} Funds Raised</ActionButton>
+                        <br />
+                        <ActionButton type='donate'>Donate To {school.schoolName}</ActionButton>
+                        <br />
+                    </Card>
                 )
             })}
         </>
